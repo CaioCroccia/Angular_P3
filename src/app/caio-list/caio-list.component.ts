0b485851add8260d981b1b89785f7afb1b7b121d
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-24 20:36:44 -0300

    2 files modified

--- src/app/caio-list/caio-list.component.ts
+++ src/app/caio-list/caio-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CaioListService } from '../caio-list.service';
 
 @Component({
   selector: 'app-caio-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaioListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public lista: CaioListService) { }
 
   ngOnInit() {
   }
