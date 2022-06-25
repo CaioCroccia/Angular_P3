a4e4538b478150fee8285537897189808eb10534
CaioCroccia <caio.carvalho15@fatec.sp.gov.br>
2022-06-24 21:44:41 -0300

    1 file modified

--- src/app/caio-list.service.ts
+++ src/app/caio-list.service.ts
@@ -1,24 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-export interface list{
-  name:string;
-  age:number;
+export interface list {
+  name: string;
+  age: number;
   counter: number;
 }
 
 @Injectable()
 export class CaioListService {
-  site='https://api.agify.io/';
-  list='';
- 
-  constructor(private http: HttpClient) { }
+  site = 'https://api.agify.io/?name=bella';
+  list = '';
 
-  getDados(){
+  constructor(private http: HttpClient) {}
+
+  getDados() {
     this.http.get<any>('https://api.agify.io/?name=bella').subscribe((data) => {
-      return this.list = data;
+      return (this.list = data);
       console.log(data);
-  })
+    });
   }
-  
 }
